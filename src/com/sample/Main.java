95b72278bce82a28e65fcfb6f3b7399fab655e2b
MyMAc <raju.skit@gmail.com>
2019-12-22 00:43:16 -0800

    PUll request example

--- src/com/sample/Main.java
+++ src/com/sample/Main.java
@@ -3,8 +3,9 @@ package com.sample;
 public class Main {
 
     public static void main(String[] args) {
-	    System.out.println("This is my first demo");
-	    System.out.println("Added another line");
-	    System.out.println("How are you doing today. I am doing fine");
-    }
+		System.out.println("This is my first demo");
+		System.out.println("Added another line");
+		System.out.println("How are you doing today. I am doing fine");
+		System.out.println("This change is to create a pull request");
+	}
 }
