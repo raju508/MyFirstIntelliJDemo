53b2ea348d77922b988232bd18c7e873820e69f1
raju508 <35345352+raju508@users.noreply.github.com>
2019-12-22 00:31:06 -0800

    Update Main.java

--- src/com/sample/Main.java
+++ src/com/sample/Main.java
@@ -5,6 +5,6 @@ public class Main {
     public static void main(String[] args) {
 	    System.out.println("This is my first demo");
 	    System.out.println("Added another line");
-	    System.out.println("How are you doing today");
+	    System.out.println("How are you doing today. I am doing fine");
     }
 }
