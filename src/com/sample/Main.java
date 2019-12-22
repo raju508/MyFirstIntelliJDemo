9601a6ea08766d6583abc70654c9f14483ac7b43
MyMAc <raju.skit@gmail.com>
2019-12-22 00:30:04 -0800

    Added one more line

--- src/com/sample/Main.java
+++ src/com/sample/Main.java
@@ -5,5 +5,6 @@ public class Main {
     public static void main(String[] args) {
 	    System.out.println("This is my first demo");
 	    System.out.println("Added another line");
+	    System.out.println("How are you doing today");
     }
 }
